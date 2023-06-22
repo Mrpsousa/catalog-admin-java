80f5cdae60ba9c5a13eccebb3a37073b78e0eddd
Mrpsousa <urameshi.uba@gmail.com>
2023-06-22 18:21:54 -0300

    feature: create entity Category

--- application/src/main/java/com/mrpsousa/admin/catalog/application/UseCase.java
+++ application/src/main/java/com/mrpsousa/admin/catalog/application/UseCase.java
@@ -1,7 +1,5 @@
 package com.mrpsousa.admin.catalog.application;
 
-import com.mrpsousa.admin.catalog.domain.Category;
-
 public class UseCase {
     public Category execute(){
         return new Category();
--- domain/src/main/java/com/mrpsousa/admin/catalog/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package com.mrpsousa.admin.catalog.domain;
-
-public class Category {
-    public String id;
-}
